254cde2d68682fbcf4f468f29069e6febd4f48c5
Robson-Ali <aliye.r@yahoo.com>
2022-09-11 08:12:35 -0700

    Advanced Task 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,27 +12,26 @@ int i, e;
 i = 48;
 e = 48;
 
-while (e < 58);
-
+while (e < 58)
+{
+	i = 48;
+	while (i < 58)
 	{
-		i = 48;
-		while (i < 58)
+		if (e != i && e < i)
 		{
-			if (e != i && e < i)
+			putchar(e);
+			putchar(i);
+			if (i == 57 && e == 56)
 			{
-				putchar(e);
-				putchar(i);
-				if (i == 57 && e == 56)
-				{
-					break;
-				}
-				putchar(',');
-				putchar(' ');
+				break;
 			}
-			i++;
+			putchar(',');
+			putchar(' ');
 		}
-		e++;
+		i++;
 	}
+	e++;
+}
 putchar('\n');
 return (0);
 }
