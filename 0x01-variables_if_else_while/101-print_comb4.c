079db3fe1ad04a0449f85e6c02fc00cc80e867a1
Robson-Ali <aliye.r@yahoo.com>
2022-09-11 10:33:40 -0700

    Advanced task 2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers between 012 to 789.
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+int r, s, t;
+
+r = 34;
+s = 34;
+t = 34;
+
+while (s < 44)
+{
+	r = 34;
+	while (r < 44)
+{
+	t = 34;
+	while (t < 44)
+	{
+		if (s != r && s != t && r != t && s < r && r < t)
+		{
+			putchar(s);
+			putchar(r);
+			putchar(t);
+			if (r == 42 && s == 41 && t == 43)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+		t++
+	}
+	r++
+}
+s++
+}
+putchar('\n');
+return (0);
+}
