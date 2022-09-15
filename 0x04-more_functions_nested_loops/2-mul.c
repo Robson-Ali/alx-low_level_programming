5dc06070beea301f28e00c7282d5fce120ac4ef5
Robson-Ali <aliye.r@yahoo.com>
2022-09-15 11:28:25 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_isdigit - checks whether a character is a digit or not
+ *@a: first number
+ *@b: second number
+ *Return: returns result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
