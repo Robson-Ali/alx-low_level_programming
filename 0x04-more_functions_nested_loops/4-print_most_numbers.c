1af7060ad8bddb9dee62ce8c402f10f3d1cf20c7
Robson-Ali <aliye.r@yahoo.com>
2022-09-15 12:26:25 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,7 +4,7 @@
 /**
 *print_most_numbers - prints numbers except 2 and 4
 *
-* Return: returns nothing
+*Return: returns nothing
 */
 
 void print_numbers(void)
@@ -13,7 +13,7 @@ void print_numbers(void)
 	
 	for (n = 48; n < 58; n++)
 	{
-		if ((n ==50) ||(n == 52))
+		if ((n == 50) || (n == 52))
 		{
 			continue;
 		}
