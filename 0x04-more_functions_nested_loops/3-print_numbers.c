0bd77112f4a17f4975f41dd1ff1f8c98e346bdce
Robson-Ali <aliye.r@yahoo.com>
2022-09-15 18:41:24 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,11 @@
 
 void print_numbers(void)
 {
-	int n;
-	
-	for (n = 48; n < 58; n++)
-	{
-		_putchar(n);
-	}
-	_putchar('\n');
+int c;
+
+for (c = 48; c < 58; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
