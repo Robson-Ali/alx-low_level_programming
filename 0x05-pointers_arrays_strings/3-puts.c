ae99e97a38f699336ae13a59d45fb6fe136e3ae2
Robson-Ali <aliye.r@yahoo.com>
2022-09-19 06:18:52 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *@str: string to print
+
+ * Description: prints a string
+ * Return: 0 (success)
+ */
+
+
+int _puts(char *str)
+
+{
+
+		int len = 0;
+
+		while (*(str + i) != '\n')
+		{
+			putchar((str + i));
+			i++:
+		}
+}
