56e9a07d6b0c7d062b4e8fdb124e549baea51a45
Robson-Ali <aliye.r@yahoo.com>
2022-09-19 08:25:19 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,18 +4,19 @@
 /**
  * _puts - prints a string
  * @str: string to print
- * 
+ *
  * Description: prints a string
  * Return: 0 (success)
  */
 
-void _puts(char *str)
+int _puts(char *str)
 {
-	int i = 0;
+	int str = 0;
 
 	while (*(str + i) != '\0')
 	{
 		putchar(*(str + i));
-		i++;
+		i++:
 	}
+	putchar(10);
 }
