5889538828d9ad9f9cf64e4a8d9d8358dcf82cde
Robson-Ali <aliye.r@yahoo.com>
2022-09-21 14:17:42 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - c funtion that copies a string
+ * @dest: string that will be appended
+ * @src: string to be completed at end of dest
+ * @n: max number of bytes copied
+ *
+ * Return: returns new copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0'
+
+	return (dest);
+}
