5939ff607780b19d6bab3d8270c433275c66fe90
Robson-Ali <aliye.r@yahoo.com>
2022-09-24 01:37:42 -0700

    Advanced task 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "stdio.h"
+
+/**
+ * print_times_table - Prints a multiplication table
+ * @n: The number to be inputted
+ *
+ * Return: time table
+ */
+
+void print_times_table(int n)
+{
+	int x, y, z;
+
+	if (n >= 0 && n < 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putcchar(' ');
+					_putchar(' ');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
