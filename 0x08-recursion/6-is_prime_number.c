476dd8ee88ae0446b8af39f53fc160f4196c74f0
Robson-Ali <aliye.r@yahoo.com>
2022-09-28 12:56:27 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -30,26 +30,26 @@ int evaluate_num(int num, int i)
 
 /**
  * is_prime_number - evaluates prime or not
- * @n: number
+ * @num: number
  *
  * Return: return 1 prime - return 0 otherwise
  */
 
-int is_prime_number(int n)
+int is_prime_number(int num)
 {
 	int i;
 
 	i = 2;
 
-	if (n < 2)
+	if (num < 2)
 	{
 		return (0);
 	}
 
-	if (n == 2)
+	if (num == 2)
 	{
 		return (1);
 	}
 
-	return (evaluate_num(n, i));
+	return (evaluate_num(num, i));
 }
