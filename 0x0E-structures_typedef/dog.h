ae621c9ed9138dec069ad9748cac5372cbf7ae90
Robson-Ali <aliye.r@yahoo.com>
2022-10-10 11:55:12 -0700

    task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,10 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog my_dog;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-my_dog *new_dog(char *name, float age, char *owner);
-void free_dog(my_dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
