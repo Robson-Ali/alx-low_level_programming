f51ca063398e5efb3e97c62bc5df93569ecf39d2
Robson-Ali <aliye.r@yahoo.com>
2022-10-12 10:08:28 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,14 +25,14 @@ int main(int argc, char *argv[])
 	}
 
 	s = argv[2][0];
-	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '+')
+	if (s != '+' && s != '-' && s != '/' && s != '*' && s != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	rob = (get_op_func(argv[2])) (a, b);
+	rob = (get_op_func(argv[2]))(a, b);
 	printf("%d\n", rob);
 	return (0);
 }
