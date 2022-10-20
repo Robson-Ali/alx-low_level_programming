a774318da3624573c7873dbaf3cbbfce05bcecd5
Robson-Ali <aliye.r@yahoo.com>
2022-10-20 13:05:05 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes;
+	size_t rob;
 
-	while (h)
+	rob = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		printf("[%d] %s\n", 0, "(nil)");
 
 		else
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 
-		nodes++;
 		h = h->next;
+		rob++;
 	}
-
-	return (nodes);
+	return (rob);
 }
