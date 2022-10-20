ab0f62831ba8d8fe38bdeee022949c069029727e
Robson-Ali <aliye.r@yahoo.com>
2022-10-20 12:59:56 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,25 +4,24 @@
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
  *
- * Return: number of robsi in the h
+ * Return: number of nodes in the list
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t robsi;
+	size_t nodes;
 
-	robsi = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)\n");
+			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
+		nodes++;
 		h = h->next;
-		robsi++;
 	}
 
-	return (robsi);
+	return (nodes);
 }
