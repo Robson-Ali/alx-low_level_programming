397b57a196d753fc44ecda99a7a16537a290b89b
Robson-Ali <aliye.r@yahoo.com>
2022-10-25 11:42:04 -0700

    Advanced map

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list.
+ * @head: head of a list.
+ *
+ * Return: pointer to the first node.
+ *
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *robson, *hanan;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	hanan = NULL;
+
+	while ((*head)->next != NULL)
+	{
+		robson = (*head)->next;
+		(*head)->next = hanan;
+		hanan = *head;
+		*head = robson;
+	}
+	(*head)->next = robson;
+	return (*head);
+}
